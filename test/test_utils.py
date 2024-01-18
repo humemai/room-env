b677bb7f825d4a50415df88414b6876010f0321c
Taewoon Kim <tae898@gmail.com>
2024-01-18 18:02:52 +0100

    sanity check

--- test/test_utils.py
+++ test/test_utils.py
@@ -5,7 +5,7 @@ from room_env.utils import argmax, get_duplicate_dicts, list_duplicates_of
 
 class UtilsTest(unittest.TestCase):
     def test_argmax(self):
-        self.assertEqual(argmax([6, 1, 2, 3, 4, 5]), 1)
+        self.assertEqual(argmax([6, 1, 2, 3, 4, 5]), 0)
 
     def test_get_duplicate_dicts(self):
         foo = get_duplicate_dicts({"foo": 1}, [{"foo": 1}, {"bar": 2}, {"foo": 1}])
